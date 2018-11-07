92f1cde74f2b2906123762ed79b7ce38f466def0
XiongAmao <onycat@live.cn>
2018-11-07 16:16:38 +0800

    :white_check_mark: add 013 roman to integer

---
+++ problems/013-roman-to-integer/testcases.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    input: ['III'],
+    output: 3
+  },
+  {
+    input: ['IV'],
+    output: 4
+  },
+  {
+    input: ['IX'],
+    output: 9
+  },
+  {
+    input: ['LVIII'],
+    output: 58
+  },
+  {
+    input: ['MCMXCIV'],
+    output: 1994
+  }
+]
