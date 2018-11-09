b848fabc0bdb87713d41ac4ee36f90b8fc952ced
XiongAmao <onycat@live.cn>
2018-11-09 17:04:01 +0800

    :white_check_mark: add 020 Valid Parentheses

---
+++ problems/020-valid-parentheses/testcases.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    input: ['()'],
+    output: true
+  },
+  {
+    input: ['()[]{}'],
+    output: true
+  },
+  {
+    input: ['(]'],
+    output: false
+  },
+  {
+    input: ['(])]'],
+    output: false
+  },
+  {
+    input: ['{[]}'],
+    output: true
+  },
+  {
+    input: ['(('],
+    output: false
+  },
+  {
+    input: ['{[[]{}]}'],
+    output: true
+  }
+]
