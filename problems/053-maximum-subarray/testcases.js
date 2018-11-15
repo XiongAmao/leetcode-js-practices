cb50df8853e174d6c38648ecb54f0c057021b2ba
XiongAmao <onycat@live.cn>
2018-11-15 21:26:02 +0800

    :white_check_mark: add 053 maximum subarray

---
+++ problems/053-maximum-subarray/testcases.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    input: [[-2,1,-3,4,-1,2,1,-5,4]],
+    output: 6
+  },
+  {
+    input: [[1]],
+    output: 1
+  },
+  {
+    input: [[-2,1]],
+    output: 1
+  },
+  {
+    input: [[-2, -1]],
+    output: -1
+  }
+]
