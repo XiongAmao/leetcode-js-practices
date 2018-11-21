a0240ae493f05fe5f06aa18ff23dc853e9981378
XiongAmao <onycat@live.cn>
2018-11-21 17:03:56 +0800

    :white_check_mark: add 088 merge sorted array

---
+++ problems/088-merge-sorted-array/index.js
@@ -0,0 +1,38 @@
+/**
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ * Note:
+ * The number of elements initialized in nums1 and nums2 are m and n respectively.
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n)
+ * to hold additional elements from nums2
+ *
+ * Example:
+ * Input:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ * Output: [1,2,2,3,5,6]
+ */
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = (nums1, m, nums2, n) => {
+  while (n > 0) {
+    // when m = 0 , condition is always false
+    if (nums1[m - 1] >= nums2[n - 1]) {
+      m--;
+      nums1[m + n] = nums1[m];
+    } else {
+      n--;
+      nums1[m + n] = nums2[n];
+    }
+  }
+};
+
+module.exports = [
+  merge
+]
---
+++ problems/088-merge-sorted-array/testcases.js
@@ -0,0 +1,23 @@
+const createCases = (input, output) => {
+  const { nums1, m, nums2, n } = input;
+  return (fn) => {
+    fn(nums1, m, nums2, n);
+    expect(nums1).toEqual(output);
+  }
+}
+
+module.exports = [
+  createCases({
+    nums1: [0],
+    m: 0,
+    nums2: [1],
+    n: 1
+  }, [1]),
+  createCases({
+    nums1: [1, 2, 3, 0, 0, 0],
+    m: 3,
+    nums2: [2, 5, 6],
+    n: 3
+  }, [1, 2, 2, 3, 5, 6])
+]
+
