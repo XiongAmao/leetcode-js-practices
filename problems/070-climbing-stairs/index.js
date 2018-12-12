f4169967bc360f71e8d9c7cf9376720e76672557
XiongAmao <onycat@live.cn>
2018-12-12 15:14:40 +0800

    :white_check_mark: add 070-climbing-stairs

---
+++ problems/070-climbing-stairs/index.js
@@ -0,0 +1,93 @@
+/**
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * Note: Given n will be a positive integer.
+ *
+ * Example 1:
+ *
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ *
+ * Example 2:
+ *
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+// too slow
+ const climbStairs = (n) => {
+  function climb (i) {
+    if (i > n) {
+      return 0;
+    };
+
+    if (i === n) {
+      return 1;
+    };
+
+    return climb(i + 1) + climb(i + 2);
+  };
+
+  return climb(0)
+};
+
+const climbStairsMemo = (n) => {
+  const memo = [];
+
+  function climb (i) {
+    if (i > n) {
+      return 0;
+    };
+
+    if (i === n) {
+      return 1;
+    };
+
+    if (memo[i] > 0) {
+      return memo[i];
+    };
+
+    memo[i] = climb(i + 1) + climb(i + 2);
+
+    return memo[i];
+  };
+  return climb(0)
+}
+
+// too slow
+const climbStairs2 = (n) => {
+  let count = 0;
+
+  function nextStep (remain) {
+    if (remain > 1) {
+      nextStep(remain - 1);
+      nextStep(remain - 2);
+    } else if (remain === 1) {
+      nextStep(remain - 1);
+    } else {
+      count++;
+    };
+  };
+
+  nextStep(n);
+
+  return count;
+};
+
+
+module.exports = [
+  climbStairsMemo
+]
---
+++ problems/070-climbing-stairs/testcases.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    input: [1],
+    output: 1
+  },
+  {
+    input: [2],
+    output: 2
+  },
+  {
+    input: [3],
+    output: 3
+  },
+  {
+    input: [4],
+    output: 5
+  },
+  {
+    input: [6],
+    output: 13
+  },
+  {
+    input: [30],
+    output: 1346269
+  },
+  {
+    input: [44],
+    output: 1134903170
+  },
+  {
+    input: [60],
+    output: 2504730781961
+  }
+]
