e521008aea5636815ddc15e47eb5705aa420c3bb
XiongAmao <onycat@live.cn>
2019-08-26 22:17:17 +0800

    Finish 027.

--- problems/027-remove-element/index.js
+++ problems/027-remove-element/index.js
@@ -4,11 +4,31 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-  for(let i = 0;)
-  console.log(nums);
+  let idx = 0;
+
+  while (idx < nums.length) {
+    if (nums[idx] === val) {
+      nums.splice(idx, 1);
+    } else {
+      idx++;
+    }
+  }
+
+  return nums.length;
+};
+
+var removeElement2 = function (nums, val) {
+  let idx = nums.length - 1;
+  while(idx >= 0) {
+    if (nums[idx] === val) {
+      nums.splice(idx, 1);
+    }
+    idx--;
+  }
   return nums.length;
 };
 
 module.exports = [
-  removeElement
+  removeElement,
+  removeElement2
 ];
