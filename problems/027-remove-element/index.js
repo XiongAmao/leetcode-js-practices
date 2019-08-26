b8deba2e01a57e24f2f0975fa8009fbbd0ba2843
XiongAmao <onycat@live.cn>
2019-08-26 18:41:58 +0800

    Add new problem

---
+++ problems/027-remove-element/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  for(let i = 0;)
+  console.log(nums);
+  return nums.length;
+};
+
+module.exports = [
+  removeElement
+];
