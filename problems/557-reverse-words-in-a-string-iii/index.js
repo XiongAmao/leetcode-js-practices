35f4730b220ffa331f9ddcff49f70466dc362702
XiongAmao <onycat@live.cn>
2019-10-03 22:09:39 +0800

    :white_check_mark: add 557 reverse words in string

---
+++ problems/557-reverse-words-in-a-string-iii/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  return s
+    .split(' ')
+    .map((str) =>
+      str
+        .split('')
+        .reverse()
+        .join('')
+    )
+    .join(' ');
+};
+
+module.exports = [reverseWords];
---
+++ problems/557-reverse-words-in-a-string-iii/test.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    input: ["Let's take LeetCode contest"],
+    output: "s'teL ekat edoCteeL tsetnoc"
+  },
+  {
+    input: ["Let'aaa1 take asf contest"],
+    output: "1aaa'teL ekat fsa tsetnoc"
+  }
+];
