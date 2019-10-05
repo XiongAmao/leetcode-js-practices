240f127b3674c28ba75d7cbfeb6f05a73b888e77
XiongAmao <onycat@live.cn>
2019-10-05 17:59:51 +0800

    :white_check_mark: add 371 sum of two intergers

---
+++ problems/371-sum-of-two-intergers/test.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    input: [1, 4],
+    output: 5
+  },
+  {
+    input: [12, 15],
+    output: 27
+  },
+  {
+    input: [-1, -10],
+    output: -11
+  },
+  {
+    input: [-1, 1],
+    output: 0
+  }
+];
