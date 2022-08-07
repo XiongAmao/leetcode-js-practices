e5daf2e8d88fcab58e95e37cbc2668044d66c4ed
XiongAmao <onycat@live.cn>
2022-08-07 22:33:29 +0800

    refactor: 027 remove element

---
+++ problems/027-remove-element/index.test.ts
@@ -0,0 +1,21 @@
+import { removeElement, removeElement2, RemoveElementFn } from '.';
+
+const testCase = (fn: RemoveElementFn) => {
+  test.each([
+    [[3, 2, 2, 3], 3, 2],
+    [[], 3, 0],
+    [[1, 1, 2, 2, 3, 4, 4, 1, 2, 5, 4, 6], 1, 9]
+  ])('Case: %#', (nums, val, resultLength) => {
+    expect(fn(nums, val)).toBe(resultLength);
+  });
+};
+
+describe('027 remove element', () => {
+  describe('remove element', () => {
+    testCase(removeElement);
+  });
+
+  describe('remove element 2', () => {
+    testCase(removeElement2);
+  });
+});
--- problems/027-remove-element/index.ts
+++ problems/027-remove-element/index.ts
@@ -1,9 +1,6 @@
-/**
- * @param {number[]} nums
- * @param {number} val
- * @return {number}
- */
-var removeElement = function(nums, val) {
+export type RemoveElementFn = (nums: number[], val: number) => number;
+
+export const removeElement: RemoveElementFn = (nums, val) => {
   let idx = 0;
 
   while (idx < nums.length) {
@@ -17,18 +14,16 @@ var removeElement = function(nums, val) {
   return nums.length;
 };
 
-var removeElement2 = function (nums, val) {
+export const removeElement2: RemoveElementFn = (nums, val) => {
   let idx = nums.length - 1;
-  while(idx >= 0) {
+
+  while (idx >= 0) {
     if (nums[idx] === val) {
       nums.splice(idx, 1);
+    } else {
+      idx--;
     }
-    idx--;
   }
+
   return nums.length;
 };
-
-module.exports = [
-  removeElement,
-  removeElement2
-];
