1ac103d9243a817eac8e7c2256495985cb2ded93
XiongAmao <onycat@live.cn>
2023-04-01 00:08:45 +0800

    feat: update test case

--- problems/146-lru-cache/index.test.ts
+++ problems/146-lru-cache/index.test.ts
@@ -37,6 +37,22 @@ const testFn = (LRUConstructor: typeof LRUCacheWithMap | typeof LRUCache) => {
       ],
       [[2], [1, 0], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]],
       [null, null, null, 0, null, -1, null, -1, 3, 4]
+    ],
+    [
+      [
+        'LRUCache',
+        'put',
+        'put',
+        'get',
+        'put',
+        'get',
+        'put',
+        'get',
+        'get',
+        'get'
+      ],
+      [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]],
+      [null, null, null, 1, null, -1, null, -1, 3, 4]
     ]
   ])('Case: %#', (actions, input, expected) => {
     const cache = new LRUConstructor(input[0][0]);
